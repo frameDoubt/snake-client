f0987d194882cc1ecf57b771d997540b9f3cd529
Brandon Wright <brandon.w.wright4@gmail.com>
2021-01-14 09:22:46 +0000

    tested use of setTimeOut() to move snek

--- client.js
+++ client.js
@@ -9,6 +9,16 @@ const connect = function() {
     console.log('Successfully connected to game server');
     conn.write('Name: bad');
   });
+  // message to the server for movement of snek
+  // conn.on("connect", () => {
+  //   let time = 1000;
+  //   for(let i = 0; i < 4; i++){
+  //     setTimeout(() => {
+  //       conn.write('Move: up');
+  //     }, time);
+  //     time += 1000;
+  //   };
+  // });
   return conn;
 };
 
