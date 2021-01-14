cc1442d01084b5bb853f05a34a890b7c3f3fea30
Brandon Wright <brandon.w.wright4@gmail.com>
2021-01-14 10:30:43 +0000

    modularized input setup function to be imported and called by play file

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on('data',(key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,23 +1,23 @@
 
 const connect = require('./client');
-
+const setupInput = require('./input');
 
 console.log('Connecting...');
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-  stdin.on('data',(key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+//   const handleUserInput = (key) => {
+//     if (key === '\u0003') {
+//       process.exit();
+//     }
+//   };
+//   stdin.on('data',(key) => {
+//     handleUserInput(key);
+//   });
+//   return stdin;
+// };
 setupInput();
